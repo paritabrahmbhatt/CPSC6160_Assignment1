097e2e59313eacaf05c3934dbf3f2e6094bba5f4
Rintu Noelmon <124462732+RintuNoelmon@users.noreply.github.com>
2023-02-19 01:56:29 -0500

    Add files via upload

---
+++ CPSC6160_Assignment1-main/README.md
@@ -0,0 +1,2 @@
+# CPSC6160_Assignment1
+CPSC 6160 2D Game Design Assignment 1
---
+++ CPSC6160_Assignment1-main/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  9 12:09:21 2023
+
+@author: parit
+"""
+
+import Game as my
+obj1 = my.Game()
+
+obj1.run()
+
+
+
+
