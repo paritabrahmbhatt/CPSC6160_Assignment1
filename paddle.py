0f0df91a0e9bdc46e1e395019823139dbaaf95fe
Parita Brahmbhatt <67082863+paritabrahmbhatt@users.noreply.github.com>
2023-03-01 21:11:46 -0500

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Feb 13 15:18:17 2023
 
-@author: parit
+@author: parita and rintu
 """
 
 import pygame 
@@ -17,4 +17,13 @@ class Paddle(pygame.sprite.Sprite):
         pygame.draw.rect(self.image, color, [0, 0, width,height])
         
         self.rect = self.image.get_rect()
-    
+
+    def moveUp(self, pixels):
+        self.rect.y -= pixels
+        if self.rect.y < 0:
+            self.rect.y = 0
+
+    def moveDown(self, pixels):
+        self.rect.y += pixels
+        if self.rect.y > 400:
+            self.rect.y = 400
