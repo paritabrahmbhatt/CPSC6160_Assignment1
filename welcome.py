342d5c5e44c336ee279e81cb04f763830d0e75f1
Parita Brahmbhatt <67082863+paritabrahmbhatt@users.noreply.github.com>
2023-03-01 20:49:33 -0500

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  1 09:45:48 2023
+
+@author: parita
+"""
+
+import screen as s
+import pygame
+
+class Welcome_Screen(s.Screen):
+    
+    pygame.init()
+    wlcm_scrn = pygame.display.set_mode(s.Screen.size)
+    pygame.display.set_caption(s.Screen.title)
+    pygame.display.set_icon(s.Screen.icon)
+    
+    
+class Button(Welcome_Screen):
+    
+    def __init__(self,x,y,img,text_input,scale_w,scale_h):
+        self.image = img
+        self.x_pos = x
+        self.y_pos = y
+        self.btn_rect = self.image.get_rect(center=(self.x_pos,self.y_pos))
+        
+        #Adding Text to the button
+        self.text_input = text_input
+        self.text = Welcome_Screen.main_font.render(self.text_input, True, (255,255,255))
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+        
+        #Resizing the image of Button
+        self.scale_h = scale_h
+        self.scale_w = scale_w
+        self.image = pygame.transform.scale(self.image,(self.scale_w,self.scale_h))
+    
+    def draw_btn(self):
+        Welcome_Screen.wlcm_scrn.blit(self.image, self.btn_rect)
+        Welcome_Screen.wlcm_scrn.blit(self.text,self.text_rect)
+           
+    def checkIp(self,position):
+        if position[0] in range(self.btn_rect.left, self.btn_rect.right) and position[1] in range(self.btn_rect.top, self.btn_rect.bottom):
+            return True
+        
+    
