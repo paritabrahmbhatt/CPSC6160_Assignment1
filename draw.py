a0b67bd61514eb979ccdaf58a13c2e4f4b439eb6
Parita Brahmbhatt <67082863+paritabrahmbhatt@users.noreply.github.com>
2023-03-01 20:50:03 -0500

    Update draw.py

--- draw.py
+++ draw.py
@@ -2,31 +2,32 @@
 """
 Created on Mon Feb 13 15:30:47 2023
 
-@author: parit
+@author: parita
 """
 
 import paddle
+import ball
 import screen as s
-import pygame
 
 
-class Draw(paddle.Paddle,s.Screen):
-    def draw(self):
-        p1 = paddle.Paddle((255,0,0),100,10)
-        p1.rect.x = 20
-        p1.rect.y = 200
+class Draw(paddle.Paddle,ball.Ball,s.Screen):
 
-        #s.Screen.scrn.blit(p1, (x, y))
-        
-        #PADDLE 2
-        
-        p2 = paddle.Paddle((255,0,0), 100, 10)
-        p2.rect.x = 670
-        p2.rect.y = 200
-        
-        all_sprites = pygame.sprite.Group()
-        all_sprites.add(p1)
-        all_sprites.add(p2)
+   #PADDLE1
 
-        all_sprites.update()
-        all_sprites.draw(s.Screen.scrn)
+   p1 = paddle.Paddle((255,255,255), 100, 10)
+   p1.rect.x = 20
+   p1.rect.y = 200
+
+   # s.Screen.scrn.blit(p1, (x, y))
+
+   # PADDLE 2
+
+   p2 = paddle.Paddle((255, 255, 255), 100, 10)
+   p2.rect.x = 670
+   p2.rect.y = 200
+
+   #BALL
+
+   ball = ball.Ball((255, 255, 255), 10, 10)
+   ball.rect.x = 350
+   ball.rect.y = 250
