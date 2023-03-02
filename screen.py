ccd4c2d7ea844583f4f4fc8bc1983015e0a7fe33
Parita Brahmbhatt <67082863+paritabrahmbhatt@users.noreply.github.com>
2023-03-01 20:50:45 -0500

    Update screen.py

--- screen.py
+++ screen.py
@@ -5,25 +5,25 @@ Created on Thu Feb  9 10:19:03 2023
 @author: parita
 """
 
-import math
-import random
-import os
 import pygame
-from pygame import mixer
-import sys
-
 
 #Generalized block of screen
 class Screen():
-    __instance = None
+    #__instance = None
     pygame.init()
     size = (700,500)
+    
+    #Giving the title
     title = "Pong"
-    icon = pygame.image.load(r'Images/pong.ico')
-    scrn = pygame.display.set_mode(size)
-    clock = pygame.time.Clock()
-    
-    
     pygame.display.set_caption(title)
+    
+    #Puttong icon of Pong
+    icon = pygame.image.load(r'Images/pong.ico')
     pygame.display.set_icon(icon)
+    
+    main_font = pygame.font.SysFont("cambria", 50)
+
+    scrn = pygame.display.set_mode(size)
+    
+    clock = pygame.time.Clock()
     
